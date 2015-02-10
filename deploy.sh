4e2ac71c64b043919336dd6dfd64c9d6d9b76e80
DD <dpd_damyanov@abv.bg>
2015-02-10 17:28:55 +0200

    Eb

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 # deploy.sh
-#! /bin/bash
+#!/bin/bash
 
 SHA1=$1
 
@@ -7,7 +7,7 @@ SHA1=$1
 docker push thedpd/api-peer:$SHA1
 
 # Create new Elastic Beanstalk version
-EB_BUCKET=dpd-peer
+EB_BUCKET=peerbelt
 DOCKERRUN_FILE=$SHA1-Dockerrun.aws.json
 #sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
 aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE
