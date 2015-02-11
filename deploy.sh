7c17a23567412273c5bb596122fcbf18e8fa9714
DD <dpd_damyanov@abv.bg>
2015-02-11 13:55:18 +0200

    dock

--- deploy.sh
+++ deploy.sh
@@ -12,8 +12,8 @@ echo thedpd | docker push thedpd/api-peer
 EB_BUCKET=peerbelt
 DOCKERRUN_FILE=Dockerrun.aws.json
 #sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
-aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE
-aws elasticbeanstalk create-application-version --application-name zeCHAT --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE
+aws s3 cp --region-names eu-west-1 $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE
+aws elasticbeanstalk create-application-version --region-names eu-west-1 --application-name zeCHAT --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE
 
 # Update Elastic Beanstalk environment to new version
-aws  elasticbeanstalk update-environment --environment-name PROD --version-label $SHA1
+aws  elasticbeanstalk update-environment --region-names eu-west-1 --environment-name PROD --version-label $SHA1
