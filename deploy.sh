ce04429f7d4f57514816ec026ce6b85f1259b65d
DD <dpd_damyanov@abv.bg>
2015-02-11 18:06:27 +0200

    docker

--- deploy.sh
+++ deploy.sh
@@ -12,8 +12,8 @@ EB_BUCKET=peerbelt
 DOCKERRUN_FILE=Dockerrun.aws.json
 #sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
 aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE
-aws elasticbeanstalk create-environment  --application-name peerbelt --environment-name TEST-$SHA1 --template-name peerbelt-temp
-aws elasticbeanstalk create-application-version --application-name peerbelt --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE
+aws elasticbeanstalk create-environment  --application-name peerbelt-api --environment-name TEST-$SHA1 --template-name peerbelt-temp
+aws elasticbeanstalk create-application-version --application-name peerbelt-api --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE
 
 # Update Elastic Beanstalk environment to new version
 aws elasticbeanstalk update-environment --environment-name TEST-$SHA1 --version-label $SHA1
