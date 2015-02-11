3abcddf0c7d7b0d69b7dabcdabcf7e766a7dc46a
DD <dpd_damyanov@abv.bg>
2015-02-11 16:10:14 +0200

    korecd

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,7 @@ EB_BUCKET=peerbelt
 DOCKERRUN_FILE=Dockerrun.aws.json
 #sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
 aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE
-aws elasticbeanstalk create-application-version --application-name zeCHAT --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE
+aws elasticbeanstalk create-application-version --application-name peerbelt-api --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE
 
 # Update Elastic Beanstalk environment to new version
 aws elasticbeanstalk update-environment --environment-name PROD --version-label $SHA1
