98917728a58a3f04d529c6b7fc828d2cd5070aba
DD <dpd_damyanov@abv.bg>
2015-02-11 12:18:45 +0200

    dockerizer

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ echo thedpd | docker push thedpd/api-peer
 
 # Create new Elastic Beanstalk version
 EB_BUCKET=peerbelt
-DOCKERRUN_FILE=$SHA1-Dockerrun.aws.json
+DOCKERRUN_FILE=Dockerrun.aws.json
 #sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
 aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE
 aws elasticbeanstalk create-application-version --application-name zeCHAT --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE
