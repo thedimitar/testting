4366964a7f97a4500dbab254479763a0ed68f10b
DD <dpd_damyanov@abv.bg>
2015-02-12 12:07:31 +0200

    !&^%#*&GH@(*!

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,11 @@ DOCKERRUN_FILE=Dockerrun.aws.json
 aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE
 aws elasticbeanstalk create-environment  --application-name peerbelt-api --environment-name TEST-$CIRCLE_BUILD_NUM --template-name peerbelt-config
 aws elasticbeanstalk create-application-version --application-name peerbelt-api --version-label $SHA1 --source-bundle S3Bucket=peerbelt,S3Key=Dockerrun.aws.json
-sleep 300;
+         COUNTER=0
+         while [  $COUNTER -lt 10 ]; do
+             echo The counter is $COUNTER
+             let COUNTER=COUNTER+1 
+	     sleep 60;
+         done
 # Update Elastic Beanstalk environment to new version
 aws elasticbeanstalk update-environment --environment-name TEST-$CIRCLE_BUILD_NUM --version-label $SHA1
