fd4deff808851e45a6077e5b795fbd8d2c3ad1de
DD <dpd_damyanov@abv.bg>
2015-02-13 11:07:12 +0200

    more testings

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,7 @@ EB_BUCKET=peerbelt
 DOCKERRUN_FILE=Dockerrun.aws.json
 #sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
 aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE
-aws elasticbeanstalk create-environment  --application-name peerbelt-api --environment-name TEST-$CIRCLE_BUILD_NUM --template-name peer-elb-as
+aws elasticbeanstalk create-environment  --application-name LB-AS-peer-conf --environment-name TEST-$CIRCLE_BUILD_NUM --template-name peer-elb-as
 aws elasticbeanstalk create-application-version --application-name LB-AS-peer-conf --version-label $SHA1 --source-bundle S3Bucket=peerbelt,S3Key=Dockerrun.aws.json
 #Create environtment takes a while, tha is why the below loop is necessary. Without it, the build will fail. 
          COUNTER=0
