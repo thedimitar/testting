81d14093ba5836ed6a622c63fed78db708ab4dab
DD <dpd_damyanov@abv.bg>
2015-02-13 11:02:10 +0200

    more testing

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 
 SHA1=$1
 
-# Deploy image to Docker Hub
+# Deploy image to private images repo. 
 
 echo thedpd | docker push thedpd/socketio
 
@@ -12,8 +12,9 @@ EB_BUCKET=peerbelt
 DOCKERRUN_FILE=Dockerrun.aws.json
 #sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
 aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE
-aws elasticbeanstalk create-environment  --application-name peerbelt-api --environment-name TEST-$CIRCLE_BUILD_NUM --template-name peerbelt-config
-aws elasticbeanstalk create-application-version --application-name peerbelt-api --version-label $SHA1 --source-bundle S3Bucket=peerbelt,S3Key=Dockerrun.aws.json
+aws elasticbeanstalk create-environment  --application-name peerbelt-api --environment-name TEST-$CIRCLE_BUILD_NUM --template-name peer-elb-as
+aws elasticbeanstalk create-application-version --application-name LB-AS-peer-conf --version-label $SHA1 --source-bundle S3Bucket=peerbelt,S3Key=Dockerrun.aws.json
+#Create environtment takes a while, tha is why the below loop is necessary. Without it, the build will fail. 
          COUNTER=0
          while [  $COUNTER -lt 10 ]; do
              echo The counter is $COUNTER
