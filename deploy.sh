5574f4c74569f0a77b53c0ccc102fb898021736a
DD <dpd_damyanov@abv.bg>
2015-02-24 12:20:18 +0200

    testing push

--- deploy.sh
+++ deploy.sh
@@ -7,19 +7,3 @@ SHA1=$1
 
 echo thedpd | docker push thedpd/socketio
 
-# Create new Elastic Beanstalk version
-EB_BUCKET=peerbelt
-DOCKERRUN_FILE=Dockerrun.aws.json
-#sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
-aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE
-aws elasticbeanstalk create-environment  --application-name LB-AS-peer-conf --environment-name TEST-$CIRCLE_BUILD_NUM --template-name peer-elb-as
-aws elasticbeanstalk create-application-version --application-name LB-AS-peer-conf --version-label $SHA1 --source-bundle S3Bucket=peerbelt,S3Key=Dockerrun.aws.json
-#Create environtment takes a while, tha is why the below loop is necessary. Without it, the build will fail. 
-         COUNTER=0
-         while [  $COUNTER -lt 10 ]; do
-             echo The counter is $COUNTER
-             let COUNTER=COUNTER+1 
-	     sleep 60;
-         done
-# Update Elastic Beanstalk environment to new version
-aws elasticbeanstalk update-environment --environment-name TEST-$CIRCLE_BUILD_NUM --version-label $SHA1
